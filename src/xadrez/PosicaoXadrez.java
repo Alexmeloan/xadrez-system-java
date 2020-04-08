f2c07f0338cc0ba6a714b7d0c59a2e0f6e0ddb95
AlexMelo79 <alexmello79@yahoo.com.br>
2020-04-08 12:02:09 -0300

    XadrezException e PosicaoXadrez

---
+++ src/xadrez/PosicaoXadrez.java
@@ -0,0 +1,48 @@
+package xadrez;
+
+import tabuleirojogo.Posicao;
+
+public class PosicaoXadrez {
+	
+	private char coluna;
+	private int linha;
+	
+	
+	
+	public PosicaoXadrez(char coluna, int linha) {
+		if(coluna < 'a' || coluna > 'h' || linha < 1 || linha > 8) {
+			throw new XadrezException("Erro ao instanciar o PosicaoXadrez. Os valores válidos são de a1 a h8"); 
+		}
+		this.coluna = coluna;
+		this.linha = linha;
+	}
+	
+	
+	public char getColuna() {
+		return coluna;
+	}
+
+
+	public int getLinha() {
+		return linha;
+	}
+
+
+	protected Posicao posicionar() {
+		return new Posicao(8 - linha, coluna - 'a');
+	}
+	protected static PosicaoXadrez apartirDePosicao(Posicao posicao) {
+		return new PosicaoXadrez ((char)('a' - posicao.getColuna()), 8 - posicao.getLinha());
+	}
+	
+	@Override
+	public String toString() {
+		return "" + coluna + linha;
+	}
+
+
+	
+	
+	
+
+}

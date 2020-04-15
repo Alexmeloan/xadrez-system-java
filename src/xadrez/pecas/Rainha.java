7eee575b4e74f56d9e4e888a4e3e8c81223f8bcc
alexmelo03 <alexmello79@yahoo.com.br>
2020-04-14 23:13:38 -0300

    Rainha

---
+++ src/xadrez/pecas/Rainha.java
@@ -0,0 +1,110 @@
+package xadrez.pecas;
+
+import tabuleirojogo.Posicao;
+import tabuleirojogo.Tabuleiro;
+import xadrez.Cor;
+import xadrez.PecaXadrez;
+
+public class Rainha extends PecaXadrez {
+
+	public Rainha(Tabuleiro tabuleiro, Cor cor) {
+		super(tabuleiro, cor);
+		
+	}
+	
+		
+	@Override
+	public boolean[][] possiveisMovimentos() {
+		boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+		
+		Posicao p = new Posicao(0, 0);
+		
+		//Acima
+		p.definirValores(posicao.getLinha() - 1, posicao.getColuna());
+		while(getTabuleiro().existePosicao(p) && !getTabuleiro().existeUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setLinha(p.getLinha() - 1);
+		}
+		if(getTabuleiro().existePosicao(p) && existePeçaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Esquerda
+		p.definirValores(posicao.getLinha(), posicao.getColuna() - 1);
+		while(getTabuleiro().existePosicao(p) && !getTabuleiro().existeUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setColuna(p.getColuna( ) - 1);
+		}
+		if(getTabuleiro().existePosicao(p) && existePeçaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+				
+		//Direita
+		p.definirValores(posicao.getLinha(), posicao.getColuna() + 1);
+		while(getTabuleiro().existePosicao(p) && !getTabuleiro().existeUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setColuna(p.getColuna( ) + 1);
+		}
+		if(getTabuleiro().existePosicao(p) && existePeçaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+				}
+				
+		//Abaixo
+		p.definirValores(posicao.getLinha() + 1, posicao.getColuna());
+		while(getTabuleiro().existePosicao(p) && !getTabuleiro().existeUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setLinha(p.getLinha() + 1);
+		}
+		if(getTabuleiro().existePosicao(p) && existePeçaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//NW
+		p.definirValores(posicao.getLinha() - 1, posicao.getColuna() -1);
+		while(getTabuleiro().existePosicao(p) && !getTabuleiro().existeUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.definirValores(p.getLinha() - 1, p.getColuna() - 1);
+		}
+		if(getTabuleiro().existePosicao(p) && existePeçaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//NE
+		p.definirValores(posicao.getLinha() - 1, posicao.getColuna() + 1);
+		while(getTabuleiro().existePosicao(p) && !getTabuleiro().existeUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.definirValores(p.getLinha() - 1, p.getColuna() + 1);
+		}
+		if(getTabuleiro().existePosicao(p) && existePeçaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+				
+		//SE
+		p.definirValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+		while(getTabuleiro().existePosicao(p) && !getTabuleiro().existeUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.definirValores(p.getLinha() + 1, p.getColuna() + 1);
+		}
+		if(getTabuleiro().existePosicao(p) && existePeçaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+				}
+				
+		//SW
+		p.definirValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
+		while(getTabuleiro().existePosicao(p) && !getTabuleiro().existeUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.definirValores(p.getLinha() + 1, p.getColuna() - 1);
+		}
+		if(getTabuleiro().existePosicao(p) && existePeçaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+				
+		    return mat;
+	     }
+	
+	@Override
+	public String toString() {
+		return "Q";
+	}
+
+}
